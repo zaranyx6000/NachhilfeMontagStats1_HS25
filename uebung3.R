b27118d4349784c0cfbd68b21c9b1316a8d1edba
zaranyx6000 <zaranyx.fm@gmail.com>
2025-10-27 08:27:11 +0100

    u 3.1-3.3

--- uebung3.R
+++ uebung3.R
@@ -7,8 +7,28 @@ semester_data2 <- read.csv("semester_data2.csv")
 # Anzeigen der ersten Zeilen des Datensatzes
 head(semester_data2)
 
-# Schreiben Sie hier ihren Code um die Aufgaben zu lösen
+# Aufgabe 3.1 ----
 
-Aufgabe 3.3
+weiblich <- semester_data2$geschlecht == "weiblich"
+IQR(semester_data2$grösse[weiblich])
+
+
+maenlich <- semester_data2$geschlecht == "männlich"
+
+IQR(semester_data2$grösse[maenlich])
+
+
+# Aufgabe 3.2 ----
+
+df_fr_it  <- semester_data2$nationalität == "Frankreich" | semester_data2$nationalität == "Italien"
+
+
+sum(weiblich & df_fr_it)
+
+# Aufgabe 3.3 -----
 index_deutsch <- semester_data2$nationalität == "Deutsch"
 range(semester_data2$alter[index_deutsch])
+
+
+
+
