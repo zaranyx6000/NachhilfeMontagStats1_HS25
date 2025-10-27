330b6d4f68a5d8ef693465332457dc27e7b1edfd
zaranyx6000 <zaranyx.fm@gmail.com>
2025-10-27 08:26:54 +0100

    uebung 3.1-3.3

--- uebung3.R
+++ uebung3.R
@@ -1,4 +1,3 @@
-# Herunterladen der Daten um sie lokal verfügbar zu machen
 url <- "https://unilu-vpf-statistik.github.io/uebung_statistik1/semester_data2.csv"
 download.file(url, "semester_data2.csv")
 
@@ -9,12 +8,7 @@ semester_data2 <- read.csv("semester_data2.csv")
 head(semester_data2)
 
 # Schreiben Sie hier ihren Code um die Aufgaben zu lösen
-# Aufgabe 3.1 ----
-weiblich <- semester_data2$geschlecht == "weiblich"
-IQR(semester_data2$grösse[weiblich])
-maenlich <- semester_data2$geschlecht == "männlich"
-IQR(semester_data2$grösse[maenlich])
 
-# Aufgabe 3.2 ----
-df_fr_it  <- semester_data2$nationalität == "Frankreich" | semester_data2$nationalität == "Italien"
-sum(weiblich & df_fr_it)
+Aufgabe 3.3
+index_deutsch <- semester_data2$nationalität == "Deutsch"
+range(semester_data2$alter[index_deutsch])
